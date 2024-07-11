f7130a7bd9ef258b0e478196cc8a1209537720d1
Michael Zavarella <mzavarella30@gmail.com>
2024-07-11 10:36:51 -0400

    (local)leader changes to support which-key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set the leader and local leader explicitly
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Make sure which-key shoes on localleader
+vim.keymap.set("n", "<localleader>", '<cmd>lua require("which-key").show(",")<cr>')
