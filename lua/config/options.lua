7c4bd6e36f750aaa226709eafbd1acbaa09d24fb
Michael Zavarella <mzavarella30@gmail.com>
2024-07-12 19:59:41 -0400

    Remove ruby, stop making which key track local leader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,3 @@
 -- Set the leader and local leader explicitly
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
-
--- Make sure which-key shoes on localleader
-vim.keymap.set("n", "<localleader>", '<cmd>lua require("which-key").show(",")<cr>')
